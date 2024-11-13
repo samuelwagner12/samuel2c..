68e140f6964efb91293888e703c2efff33bcd1c7
samuelwagner12 <162448824+samuelwagner12@users.noreply.github.com>
2024-11-13 08:24:02 -0300

    Add files via upload

---
+++ samuelw2c/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é o warzone',
+    'O warzone é um jogo de tiro (battle royale) onde seu objetivo é ser o ultimo sobrevivente'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de portugal?',
+    'A capital de portugal é lisboa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz falar em Inglês?',
+    'falar em ingles é speak (SPEAK)'
+)
